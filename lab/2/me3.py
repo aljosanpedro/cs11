3d8341c07fb5be9f98c6a68443a4f61f35c6434b
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-17 15:59:01 +0800

    fizzbuzz optimizing

--- lab/2/me3.py
+++ lab/2/me3.py
@@ -3,16 +3,15 @@ B = int(input("End (int): "))
 
 
 for i in range(A,B+1,1):
-    result = i
-    
-    if (i % 3 == 0) or (i % 5 == 0):
-        result = ""
-    
+    result = ""
     
     if i % 3 == 0:
         result += "FIZZ"
     if i % 5 == 0:
         result += "BUZZ"
+        
+    if result == "":
+        result = i
     
     
     print(result)
