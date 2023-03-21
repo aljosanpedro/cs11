207e76b51b9b36586c84044958ebff33843806c5
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-21 12:23:39 +0800

    overhaul fib problem using lists

--- lab/2/me6.py
+++ lab/2/me6.py
@@ -1,14 +1,33 @@
+"""
+i = 0,1,2,3,4,5
+f = 1,1,2,3,5,8
+    a b c
+      a b c
+        a b c
+a = fib[0] -> fib[i]
+b = fib[1] -> fib[i+1]
+c = fib[2] -> fib[i+2]
+"""
+
 n = int(input())
 
-a,b = 0,1
+fib = [1,1]
 
 
-print(b)
+for i in range(n):
+    fib.append(fib[i] + fib[i+1])
+    
+    
+    print(fib[i])
+    
+"""
+# old version
+n = int(input())
+a,b = 1,1
 
 for i in range(n-1):
     c = a + b
-
     a,b = b,c
 
-    
-    print(c)
+    print(a)
+"""
