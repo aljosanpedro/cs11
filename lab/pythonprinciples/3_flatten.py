8bcea911bfd7a02138612be0e9e568dc6f218746
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-25 22:53:49 +0800

    Update 3_flatten.py

--- lab/pythonprinciples/3_flatten.py
+++ lab/pythonprinciples/3_flatten.py
@@ -8,9 +8,10 @@ def flatten(list_2d):
     list_2d_length = len(list_2d)
     
     for sublist_index in range(list_2d_length):
-        sublist = list_2d_length[sublist_index]
+        sublist = list_2d[sublist_index]
+        sublist_length = len(sublist)
         
-        for element_index in range(sublist):
+        for element_index in range(sublist_length):
             element = list_2d[sublist_index][element_index]
             
             flat_list.append(element)
