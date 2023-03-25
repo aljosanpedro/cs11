0433002e2822613b820e5fb9dd4c902805e4af8e
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-26 06:38:05 +0800

    Update 4_all_equal.py

--- lab/pythonprinciples/4_all_equal.py
+++ lab/pythonprinciples/4_all_equal.py
@@ -5,17 +5,19 @@ def main():
 def all_equal(list):
     is_all_equal = True
     
-    list_range = range(len(list))
-    list_max_index = list_length - 1
+    list_length = len(list)
+    list_break_index = list_length - 1
     
-    for index in list_range:
+    for index in range(list_length):
         
-        if index < list_max_index:
-            current_element = list[index]
-            next_element = list[index + 1]
+        if index == list_break_index:
+            break
             
-            if not current_element == next_element:
-                is_all_equal = False
+        current_element = list[index]
+        next_element = list[index + 1]
+            
+        if not current_element == next_element:
+            is_all_equal = False
 
     return is_all_equal
 
