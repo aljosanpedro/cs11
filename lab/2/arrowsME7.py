03a2ac20be49827f229f18280293ffe38fcc296a
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-26 12:37:55 +0800

    Update arrowsME7.py

--- lab/2/arrowsME7.py
+++ lab/2/arrowsME7.py
@@ -2,32 +2,42 @@
 print("The program will ask for a positive odd number of rows.")
 print("It will then print a pyramid with that many rows.\n")
 
-while True:
-    BASE = float(input("How many rows [positive odd integer]?: "))
-    
-    if (BASE <= 0 or 
-        BASE % 2 == 0 or 
-        not BASE.is_integer()):
-        
-        print("Invalid input.\n")
-    else:
-        break
-    
+BASE = get_base()
+
 
 # PROCESS, OUTPUT (prints)
 print(f"\nOutput when BASE = {int(BASE)}:")
 
-
 HALF = int((BASE / 2) + 1)
 
-for i in range(1,HALF,1):
-    row = '*'
-    row *= i
+for top_index in range(1,HALF,1):
+    print_row(top_index)
     
-    print(row)
+for bottom_index in range(HALF,0,-1):
+    print_row(bottom_index)
     
-for i in range(HALF,0,-1):
+
+# FUNCTIONS
+def get_base():
+    BASE = 0
+    
+    while True:
+        BASE = float(input("How many rows [positive odd integer]?: "))
+        
+        if (BASE <= 0 or 
+            BASE % 2 == 0 or 
+            not BASE.is_integer()):
+            
+            print("Invalid input.\n")
+            continue
+        
+        break
+    
+    return BASE
+    
+
+def print_row(row_length):
     row = '*'
-    row *= i
+    row *= row_length
     
     print(row)
