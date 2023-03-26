a507367e121de16efa2b1b02e62c48554702bc87
Alejandre Jose Ranoco San Pedro <ajrsanpedro215@gmail.com>
2023-03-26 12:41:58 +0800

    Update arrowsME7.py

--- lab/2/arrowsME7.py
+++ lab/2/arrowsME7.py
@@ -1,20 +1,21 @@
-# INPUT
-print("The program will ask for a positive odd number of rows.")
-print("It will then print a pyramid with that many rows.\n")
-
-BASE = get_base()
-
-
-# PROCESS, OUTPUT (prints)
-print(f"\nOutput when BASE = {int(BASE)}:")
-
-HALF = int((BASE / 2) + 1)
-
-for top_index in range(1,HALF,1):
-    print_row(top_index)
+def main():
+    # INPUT
+    print("The program will ask for a positive odd number of rows.")
+    print("It will then print a pyramid with that many rows.\n")
     
-for bottom_index in range(HALF,0,-1):
-    print_row(bottom_index)
+    BASE = get_base()
+    
+    
+    # PROCESS, OUTPUT (prints)
+    print(f"\nOutput when BASE = {int(BASE)}:")
+    
+    HALF = int((BASE / 2) + 1)
+    
+    for top_index in range(1,HALF,1):
+        print_row(top_index)
+        
+    for bottom_index in range(HALF,0,-1):
+        print_row(bottom_index)
     
 
 # FUNCTIONS
@@ -41,3 +42,6 @@ def print_row(row_length):
     row *= row_length
     
     print(row)
+    
+    
+main()
